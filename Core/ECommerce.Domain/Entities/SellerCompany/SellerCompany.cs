bdda496b180edee82a0a08df461295d6ba282f6d
Fahmin Gurbanli <gurbanlifahmin21@gmail.com>
2025-11-12 17:50:04 +0400

    Created some entities, will change.

---
+++ Core/ECommerce.Domain/Entities/SellerCompany/SellerCompany.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Domain.Common;
+
+
+namespace ECommerce.Domain.Entities.SellerCompany
+{
+    public class SellerCompany : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string IsActive { get; set; }
+        public Guid ProductId { get; set; }
+        public Product.Product Product { get; set; }
+    }
+}
