17a1c1a124d68b3468af009b661c76a552fccecc
Fahmin Gurbanli <gurbanlifahmin21@gmail.com>
2025-12-29 17:52:59 +0400

    Jwt configurations added

--- Core/ECommerce.Application/Interfaces/Tokens/ITokenService.cs
+++ Core/ECommerce.Application/Interfaces/Tokens/ITokenService.cs
@@ -8,6 +8,6 @@ namespace ECommerce.Application.Interfaces.Tokens
     {
         Task<JwtSecurityToken> CreateToken(User user, IList<string> roles);
         string GenerateRefreshToken();
-        ClaimsPrincipal? GetPrincipalFromExpiredToken();
+        ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token);
     }
 }
--- Infrastructure/ECommerce.Infrastructure/Registration.cs
+++ Infrastructure/ECommerce.Infrastructure/Registration.cs
@@ -1,7 +1,11 @@
 ﻿
+using ECommerce.Application.Interfaces.Tokens;
 using ECommerce.Infrastructure.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace ECommerce.Infrastructure
 {
@@ -10,6 +14,28 @@ namespace ECommerce.Infrastructure
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+
+            services.AddTransient<ITokenService, TokenService>();
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+            {
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                    ValidateLifetime = false,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
         }
     }
 }
