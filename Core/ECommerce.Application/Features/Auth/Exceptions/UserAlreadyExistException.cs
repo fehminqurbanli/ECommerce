b4f1b3e91ba261a92ca803bd2dba4a5d2203e70c
Fahmin <Fahmin@DESKTOP-67B6857>
2026-01-02 19:30:14 +0400

    Register endpoint created. Also for registering Validator class was created.

---
+++ Core/ECommerce.Application/Features/Auth/Exceptions/UserAlreadyExistException.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Application.Bases;
+
+namespace ECommerce.Application.Features.Auth.Exceptions
+{
+    public class UserAlreadyExistException: BaseException
+    {
+        public UserAlreadyExistException() : base("User already exists.")
+        {
+        }
+    }
+}
