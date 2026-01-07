d73be18558783ec435ade8ce1d9ef9532adb0192
Fahmin Gurbanli <gurbanlifahmin21@gmail.com>
2026-01-07 18:48:17 +0400

    revoke and revokeAll endpoints added. They works are for logging out from the system which make refreshToken of user or users null.

---
+++ Core/ECommerce.Application/Features/Auth/Command/RevokeAll/RevokeAllCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Application.AutoMapper;
+using ECommerce.Application.Bases;
+using ECommerce.Application.Interfaces.UnitOfWorks;
+using ECommerce.Domain.Entities.User;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Application.Features.Auth.Command.RevokeAll
+{
+    public class RevokeAllCommandHandler : BaseHandler, IRequestHandler<RevokeAllCommandRequest,Unit>
+    {
+        private readonly UserManager<User> _userManager;
+        public RevokeAllCommandHandler(IMapper mapper, 
+            IUnitOfWork unitOfWork, 
+            IHttpContextAccessor httpContextAccessor, 
+            UserManager<User> userManager)
+            : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Unit> Handle(RevokeAllCommandRequest request, CancellationToken cancellationToken)
+        {
+            var users = await _userManager.Users.ToListAsync(cancellationToken);
+
+            foreach (var user in users)
+            {
+                user.RefreshToken = null;
+                await _userManager.UpdateAsync(user);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Core/ECommerce.Application/Features/Auth/Command/RevokeAll/RevokeAllCommandRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ECommerce.Application.Features.Auth.Command.RevokeAll
+{
+    public class RevokeAllCommandRequest: IRequest<Unit>
+    {
+    }
+}
